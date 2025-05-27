395b26252a55517674460f9cb5011424776596ac
ThibaultSau <sauron.thibault@gmail.com>
2025-05-27 22:26:58 +0000

    no image_path used

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -110,9 +110,9 @@ class CycleGANModel(BaseModel):
         The option 'direction' can be used to swap domain A and domain B.
         """
         AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        self.real_A = input['A' if AtoB else 'B'].float().to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].float().to(self.device)
+        #self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
