963744f1650d952291ffde83aba85d8221d87b7d
ThibaultSau <thibault.sauron@gmail.com>
2025-06-20 10:07:04 +0000

    replaced ct with ex_vivo

--- data/exvivo_dataset.py
+++ data/exvivo_dataset.py
@@ -11,33 +11,43 @@ from tqdm import tqdm
 
 from skimage.transform import resize
 
-class CTDataset(BaseDataset):
+class ExVivoDataset(BaseDataset):
+        
     def __init__(self, opt):
         BaseDataset.__init__(self, opt)
         self.size = opt.load_size
         self.folder = Path(opt.dataroot)
+        self.min_clip = opt.min_clip
+        self.max_clip = opt.max_clip
         self.A = self.folder/"A"/opt.phase
         self.B = self.folder/"B"/opt.phase
         self.data={"A":[],"B":[]}
         for file in tqdm(self.A.iterdir(),desc="preloading dataset"):
             self.load_data(file)
+    
+    def clip(self,image):
+        return np.clip(image, self.min_clip, self.max_clip)
+    
+    def normalize(self,image):
+        return (self.clip(image) - self.min_clip) / (self.max_clip-self.min_clip)  # Normalize to [0,1]
+
+    def unnormalize(self,image):
+        return (image*(self.max_clip-self.min_clip) + self.min_clip)
         
     def load_data(self,file):
         a = itk.GetArrayFromImage(itk.imread(file,itk.SS))
         b = itk.GetArrayFromImage(itk.imread(self.B/file.name,itk.SS))
         
+        a = self.normalize(a)
+        b = self.normalize(b)
+
         if self.size is not None:
             a = resize(a,(a.shape[0],self.size,self.size))
             b = resize(b,(b.shape[0],self.size,self.size))
-        a-=a.min()
-        a=np.expand_dims(a/a.max(), 1)
-        
-        b-=b.min()
-        b=np.expand_dims(b/b.max(), 1)
         
         for i in range(a.shape[0]):
-            self.data["A"].append(a[i])
-            self.data["B"].append(b[i])
+            self.data["A"].append(a[None,i,:,:])
+            self.data["B"].append(b[None,i,:,:])
         
 
 
